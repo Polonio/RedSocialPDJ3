66c63d085f904a6428cd19d668c886205f7fb87a
pablomateo <p.mateo@355berrystreet.com>
2017-09-29 11:07:30 +0200

    Pablo - Firebase Database

--- app/src/main/java/es/cice/redsocialpdj3/MainContentActivity.java
+++ app/src/main/java/es/cice/redsocialpdj3/MainContentActivity.java
@@ -2,6 +2,16 @@ package es.cice.redsocialpdj3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainContentActivity extends AppCompatActivity {
 
@@ -9,5 +19,50 @@ public class MainContentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_content);
+
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+        DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+
+        database.child("user").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                List users = new ArrayList();
+                for(DataSnapshot userDataSnapshot : dataSnapshot.getChildren()) {
+
+                    String name = userDataSnapshot.child("nombre").toString();
+                    Log.d("TAG", "" + userDataSnapshot);
+                    Log.d("TAG", "" + name);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
     }
 }
+
+
+/*
+
+// Read from the database
+myRef.addValueEventListener(new ValueEventListener() {
+    @Override
+    public void onDataChange(DataSnapshot dataSnapshot) {
+        // This method is called once with the initial value and again
+        // whenever data at this location is updated.
+        String value = dataSnapshot.getValue(String.class);
+        Log.d(TAG, "Value is: " + value);
+    }
+
+    @Override
+    public void onCancelled(DatabaseError error) {
+        // Failed to read value
+        Log.w(TAG, "Failed to read value.", error.toException());
+    }
+});
+ */
