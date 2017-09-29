abb6ae4b26b6e59cd12adfbd7e6f91d8769e6f38
Diegomi1233 <mittedliego@gmail.com>
2017-09-29 11:53:36 +0200

    Base de Datos ejemplo

--- app/src/main/java/es/cice/redsocialpdj3/MainContentActivity.java
+++ app/src/main/java/es/cice/redsocialpdj3/MainContentActivity.java
@@ -3,6 +3,10 @@ package es.cice.redsocialpdj3;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -15,23 +19,33 @@ import java.util.List;
 
 public class MainContentActivity extends AppCompatActivity {
 
+    Button button_cambio;
+    EditText editTextCambio;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_content);
 
+
+
+
         FirebaseDatabase.getInstance().setPersistenceEnabled(true);
-        DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+        final DatabaseReference database = FirebaseDatabase.getInstance().getReference();
 
         database.child("user").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 List users = new ArrayList();
                 for(DataSnapshot userDataSnapshot : dataSnapshot.getChildren()) {
+                    EditText editText = (EditText) findViewById(R.id.edittext_change);
 
-                    String name = userDataSnapshot.child("nombre").toString();
+                    String name = userDataSnapshot.child("nombre").getValue().toString();
                     Log.d("TAG", "" + userDataSnapshot);
                     Log.d("TAG", "" + name);
+
+
+
+
                 }
 
             }
@@ -42,8 +56,19 @@ public class MainContentActivity extends AppCompatActivity {
             }
         });
 
+         button_cambio = (Button)findViewById(R.id.button_change);
+         editTextCambio = (EditText)findViewById(R.id.edittext_change);
 
+         button_cambio.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 database.child("user/2").child("nombre").setValue(editTextCambio.getText().toString());
+             }
+         });
     }
+
+
+
 }
 
 
